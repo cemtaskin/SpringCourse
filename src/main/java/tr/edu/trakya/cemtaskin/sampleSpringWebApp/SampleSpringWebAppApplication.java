6eecb30560c7de1df427ba904bd5b6f867c7e15b
Cem TASKIN <cemtaski82@gmail.com>
2021-03-22 09:46:57 +0300

    Spring Course - week 02

---
+++ src/main/java/tr/edu/trakya/cemtaskin/sampleSpringWebApp/SampleSpringWebAppApplication.java
@@ -0,0 +1,16 @@
+package tr.edu.trakya.cemtaskin.sampleSpringWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tr.edu.trakya.cemtaskin.sampleSpringWebApp.domain.Author;
+import tr.edu.trakya.cemtaskin.sampleSpringWebApp.domain.Book;
+
+@SpringBootApplication
+public class SampleSpringWebAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SampleSpringWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/edu/trakya/cemtaskin/sampleSpringWebApp/SampleSpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.trakya.cemtaskin.sampleSpringWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
